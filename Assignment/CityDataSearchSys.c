525d2ce920159d90cc6ee6f29b3b63675bf7c80b
qinghai <qinghai1027@gmail.com>
2025-06-08 19:15:13 +0900

    finish all function

--- Assignment/CityDataSearchSys.c
+++ Assignment/CityDataSearchSys.c
@@ -30,6 +30,82 @@ typedef struct cities{
     struct cities *nextCityNode;
 }cities;
 
+typedef struct cityList{
+    cities *head;
+    cities *end;
+    cities *current;
+}cityList;
+
+double getValue(cities* node, int n) {
+    if (node == NULL || node->this == NULL) {
+        return 0.0;
+    }
+
+    switch (n) {
+        case 0:  
+            return (double)node->this->pop;
+        case 1:  
+            return (double)node->this->lat;
+        case 2:  
+            return (double)node->this->lng;
+        default: 
+            return (double)node->this->pop;
+    }
+};
+
+void sortAdd(cities *newCitiNode,cityList *sortedCity,int n){
+    if(sortedCity -> head == NULL){
+        sortedCity -> current = newCitiNode;
+        sortedCity -> head = newCitiNode;
+        sortedCity -> end  = newCitiNode;
+    }
+
+    double new = getValue(newCitiNode, n);
+    double first = getValue(sortedCity -> head, n);
+    double end = getValue(sortedCity -> end, n);
+
+    if(new <= first){// new is the smallest
+        newCitiNode -> nextCityNode = sortedCity -> head;    
+        
+        sortedCity -> head -> preCityNode = newCitiNode;
+        sortedCity -> head = newCitiNode;
+        return;
+
+    }
+
+    if(new > end){
+        newCitiNode -> preCityNode = sortedCity -> end; 
+        
+        sortedCity -> end -> nextCityNode = newCitiNode;
+        sortedCity -> end = newCitiNode;
+        return;
+    }
+    //printf("new: %d\n", new);
+    cities *temp = sortedCity -> head;
+    double now = getValue(temp, n);
+    while (now < new)
+    {
+        temp = temp -> nextCityNode;
+        now = getValue(temp, n);
+
+
+    }
+    //printf("now: %d\n", now);
+    if(temp != NULL){
+        //printf("test2\n");
+        //new is smaller than now => add to left
+        //modify now node
+        newCitiNode -> preCityNode = temp -> preCityNode;
+        newCitiNode -> nextCityNode = temp;
+        //modify pre
+        temp -> preCityNode -> nextCityNode = newCitiNode;
+        //modify next
+        temp -> preCityNode = newCitiNode;
+        return;
+
+    }
+};
+
 void addCitiesLeft(cities *nowCities, city *nowCity, cities *firstCities){
     cities *newCities = malloc(sizeof(cities));
     newCities -> preCityNode = nowCities -> preCityNode;
@@ -67,6 +143,7 @@ void print(city *now){
         printf("City Name:%s, lat:%f lng:%f [%s] Population:%d\n",now -> cityName, now -> lat, now -> lng,
         now -> countryName, now -> pop);
 }
+
 /*
 Use the FNV-1a hash algorithm
 refrence link: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
@@ -149,68 +226,107 @@ void print_city(char *cityName){
     
 }
 
-//qs4 TODO::search function_2 : input a name of country and show all the city in this country by people
-void print_cities(char *countryName){
-    int cityHashkey = hash(countryName) % HASHSIZE;
-    city *nowCity = countryHashtable[cityHashkey];
+cityList* sort_cities(char *countryName, int dataType){
+    int cityHashkey = hash(countryName) % HASHSIZE; //find buscket
+    city *nowCity = countryHashtable[cityHashkey]; 
+
+    //ini sortcitylist
+    cityList *sortedCity = malloc(sizeof(cityList)); 
+    sortedCity -> current = NULL;
+    sortedCity -> head = NULL;
+    sortedCity -> end = NULL;
+    
     while (nowCity -> nextCityinCountry != NULL)
     {
-        if(strcmp(nowCity -> countryName, countryName) == 0)
-        print(nowCity);
-        nowCity = nowCity -> nextCityinCountry;
-    }
-    return ;
-    
-    cities *firstCities = malloc(sizeof(cities));
-    cities *lastCities = malloc(sizeof(cities));
-    cities *nowCities = malloc(sizeof(cities));
-    nowCities -> this = NULL;
-    //sort add TODO:插入的输出结果不对
-    while (strcmp(nowCity -> countryName, countryName) == 0){
-        //printf("nowcityis:");
-        print(nowCity);
-        if(nowCities -> this == NULL){
-            nowCities -> this = nowCity;
-            nowCities -> preCityNode = NULL;
-            nowCities -> nextCityNode = NULL;
-            firstCities = nowCities;
-            lastCities = nowCities;
-        }else{
-            while (nowCities -> this -> pop > nowCity -> pop){//if add one is less than Node search to left
-                //no leftnode || add > leftnode  => add = left
-                if(nowCities -> preCityNode == NULL || nowCity -> pop > nowCities -> preCityNode -> this -> pop){
-                    addCitiesLeft(nowCities, nowCity, firstCities);
-                    break;
-                }else{ //if not move to left node 
-                    nowCities = nowCities -> preCityNode;
-                }
-            }
-            while (nowCity -> pop > nowCities -> this -> pop){//if add one is more than Node search to right
-                //no rightnode || add < rightnode  => add = right
-                if(nowCities -> nextCityNode == NULL || nowCity -> pop < nowCities -> nextCityNode -> this -> pop ){
-                    addCitiesRight(nowCities, nowCity, lastCities);
-                    break;
-                }else { //if not move to right node 
-                    nowCities = nowCities -> nextCityNode;
-                }
-            }
+        if(strcmp(nowCity -> countryName, countryName) == 0){  //if nowcity is in this country
+            //TODO : creaate a new citynode
+            cities *newCitiNode = malloc(sizeof(cities));
+            newCitiNode -> this = nowCity;
+            newCitiNode -> preCityNode = NULL;
+            newCitiNode -> nextCityNode = NULL;
+            // add new citynode to sorted list
+
+            sortAdd(newCitiNode, sortedCity, dataType); //sort by list
         }
         nowCity = nowCity -> nextCityinCountry;
     }
 
-    //print
-    nowCities = firstCities;
-    while (nowCities -> nextCityNode != NULL){    
-        print(nowCities -> this);
-        nowCities = nowCities -> nextCityNode;
-    }
-    
+    return sortedCity;
 }
+//qs4 TODO::search function_2 : input a name of country and show all the city in this country by people
+void print_cities(char *countryName){
+    cityList *sortedCity = sort_cities(countryName, 0); //sort by 0 = pop
+    sortedCity -> current = sortedCity -> end;
+    while (sortedCity -> current != NULL){
+        print(sortedCity -> current -> this);
+        sortedCity -> current = sortedCity -> current -> preCityNode;
+    }
+    return ;
+    //TODO free;
+}
+
 
 //qs5 TODO::search function_3 : custom the input and show info as the commend way
 //人口順(0) or 緯度順(1) or 経度順の指定(2)と，昇順(0)or降順(1)の指定と表示開始都市名を引数に追加し，指定された並びで都市を表示できるように変更したprint_cities2()を作成せよ．
 //例: print_cities2("Japan", 0, 1, "Noda"); この場合はNodaより人口が少ない日本の都市をNodaから順に表示する．
+void print_cities2(char *countryName, int dataType, int order, char *startCity){
+    cityList *sortedCity = sort_cities(countryName, dataType); 
 
+    if (sortedCity == NULL || sortedCity->head == NULL) {
+        printf("No cities found for country: %s\n", countryName);
+        if (sortedCity != NULL) {
+            //TODO : free_cityList(sortedCity); 
+        }
+        return;
+    }
+
+    //find startNode
+    cities *startNode = NULL;
+
+    if(startCity == NULL || strcmp(startCity, "") == 0){
+        if(order == 0){
+            startNode = sortedCity -> head;
+        }else{
+            startNode = sortedCity -> end;
+        }
+    }else{
+        cities *curNode = sortedCity -> head;
+
+        while (curNode != NULL){         
+            
+            if(strcmp(curNode -> this -> cityName, startCity) == 0){
+
+                startNode = curNode;
+                break;
+            }
+
+            curNode = curNode -> nextCityNode;
+        }
+
+    }
+
+    //print by order
+    if(startNode == NULL){
+        printf("startNode error\n");
+    }else{
+        cities *current = startNode;
+
+        if(order == 0){
+            while (current != NULL){
+                print(current -> this);
+                current = current -> nextCityNode;
+            }
+        }else if(order == 1){
+            while (current != NULL){
+                print(current -> this);
+                current = current -> preCityNode;
+            }
+        }else{
+            printf("order error\n");
+        }
+    }
+    return ;
+}
 
 //sys opreat info
 void sysMenuJp(){
@@ -252,11 +368,10 @@ int main(){
     dist();
     //printf("City name? ");
     //scanf("%s", buffer);
-    print_cities("Japan"); 
+    //print_cities("Japan"); 
     //char inputBuffer[1024];
-    
-    //qs1 TODO::read file
-    //readfile();
+    //print_cities2("China", 0, 1, "Chongqing");
+
 
     //TODO::trans to hashtable
 
